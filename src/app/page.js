b759667ff8c67ab99f2a6c4051a361d5a7637a68
abdullah <abdallhameed2005@gmail.com>
2024-10-03 11:22:23 +0300

    f

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start  h-screen w-screen">
       <header className="bg-black ">
-        <h1 className="text-center text-5xl bg-black ">WELCOME TO KOUtop made BY ABDULLAH HAN (FOX)</h1>
+        <h1 className="text-center text-5xl bg-black ">WELCOME TO SelçuksikenHD</h1>
       </header>
       <iframe width="100%" height="100%" src="https://iframeri.strmrdrfrooc.click/index.php?id=3450#poster=https%3A%2F%2Fwww.selcuksportshd1374.xyz%2Fimg%2Fg%2Fcrtswp-s-min.png&amp;reklamResim=https%3A%2F%2Fwww.selcuksportshd1374.xyz%2Fimg%2Fg%2Fpshg-min-min.gif&amp;reklamGidis=https%3A%2F%2Fcutt.ly%2Fselcuksportspsh" id="iframe" title="Video Oynatıcı" frameborder="0" scrolling="no" allowfullscreen="" ></iframe>
       </main>
