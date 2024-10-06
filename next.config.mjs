7709b2108eff8ee2edaa03857d017cdb04b044dc
abdullah <abdallhameed2005@gmail.com>
2024-10-06 18:01:38 +0300

    trying to fix it

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,4 @@ import {withNextVideo} from "next-video/process"
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withNextVideo(nextConfig);
+export default nextConfig;
